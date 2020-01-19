e3149d5c455a7374e1a8f214282edc937ac057a1
Sergii Deshunin <deshunin@seas.upenn.edu>
2020-01-19 09:05:23 +0200

    Changed the end.c and added c-example.c and Make file

--- end.c
+++ end.c
@@ -1,8 +1,18 @@
 /* end.c file */
 
 #include<stdio.h>
+#include<stdlib.h>
+
+int sum (int x, int y) {
+	int static funcounter = 0;
+	funcounter++;
+	return(funcounter + x+ y);
+}
 
 int main(int argc, char** argv) {
-    printf("Hello, this is a end start");
+	int x = 0, y = 0;
+	x = sum(1,2);
+	y = sum(3,4);
+    printf("x = %d\ny = %d\n", x, y);
     return 0;
 }
